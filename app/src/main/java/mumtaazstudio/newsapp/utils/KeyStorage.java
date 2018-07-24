d495f759f187e08a1d10a91734d75e5d436cbf6f
aswanabidin <aswanabidin@ymail.com>
2018-07-25 05:40:02 +0700

    Initial commit

---
+++ app/src/main/java/mumtaazstudio/newsapp/utils/KeyStorage.java
@@ -0,0 +1,35 @@
+package mumtaazstudio.newsapp.utils;
+
+public class KeyStorage {
+
+    public static final String API_KEY = "99c4974da9f543bf85fdb49343036735";
+    public static final String EVERYTHING_END_POINT = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=99c4974da9f543bf85fdb49343036735";
+    public static final String SOURCES_END_POINT = "https://newsapi.org/v2/sources?apiKey=99c4974da9f543bf85fdb49343036735";
+
+    public final static int ALL_SORUCES_PARSING_CODE = 100;
+    public final static int ALL_TECH_SOURCES_PARSING_CODE = 200;
+
+    public final static String KEY_CATEGORY_TECH = "technology";
+    public final static String KEY_URL_TAG = "key_url";
+    public final static String KEY_URL_TO_IMAGE_TAG = "key_url_to_image";
+
+
+    //SOURCE
+    public final static String KEY_SOURCE_ID = "id";
+    public final static String KEY_SOURCE_NAME = "name";
+    public final static String KEY_SOURCE_DESCRIPTION = "description";
+    public final static String KEY_SOURCE_URL = "url";
+    public final static String KEY_SOURCE_CATEGORY = "category";
+    public final static String KEY_SOURCE_LANGUAGE = "language";
+    public final static String KEY_SOURCE_COUNTRY = "country";
+
+
+    //REQUEST CODES
+    public final static int KEY_ARTICLE_REQUEST = 200;
+    public final static int KEY_SOURCES_REQUEST = 300;
+
+
+
+
+
+}
